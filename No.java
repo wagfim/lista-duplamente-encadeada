e4395527e07745413968fba9d1b30acf6d6e8ee7
Wagner Bonfim <wagner.bfm@gmail.com>
2019-12-05 16:57:05 -0300

    Create No.java

---
+++ No.java
@@ -0,0 +1,33 @@
+public class No {
+    private int dado;
+    private No anterior;
+    private No proximo;
+
+    public No(int dado) {
+        this.dado = dado;
+    }
+
+    public int getDado() {
+        return dado;
+    }
+
+    public void setDado(int dado) {
+        this.dado = dado;
+    }
+
+    public No getAnterior() {
+        return anterior;
+    }
+
+    public void setAnterior(No anterior) {
+        this.anterior = anterior;
+    }
+
+    public No getProximo() {
+        return proximo;
+    }
+
+    public void setProximo(No proximo) {
+        this.proximo = proximo;
+    }
+}
